ba329079129996b07e3aef3bc7db3663fc66c0be
zarcut9919 <league@WTS14.attlocal.net>
2017-02-24 11:55:00 -0800

    day 5

--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -1,8 +1,8 @@
 package day5;
 
 import java.applet.AudioClip;
+import java.awt.Color;
 import java.awt.Dimension;
-
 import java.awt.Graphics;
 import java.awt.Robot;
 import java.awt.event.MouseEvent;
@@ -16,20 +16,22 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
-	
+
 	BufferedImage maze;
 	final int frameWidth = 600;
 	final int frameHeight = 400;
 
 	ScaryMaze() throws Exception {
-		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("creepy-noise.wav"));
+		sound.play();
+		// 1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
 		maze = ImageIO.read(getClass().getResource("maze.png"));
-		//2. set the mouse pointer to the start of your maze using:
-		//new Robot().mouseMove(xPosition, yPosition)
-		
-		//3. add a mouse motion listener using:
-		//addMouseMotionListener(this)
-		
+		// 2. set the mouse pointer to the start of your maze using:
+		// new Robot().mouseMove(xPosition, yPosition)
+		new Robot().mouseMove(300, 200);
+		// 3. add a mouse motion listener using:
+		// addMouseMotionListener(this)
+		addMouseMotionListener(this);
 	}
 
 	@Override
@@ -37,28 +39,34 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseX = e.getX();
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
-		//4. print the mouseColor variable to see what color the mouse is touching
+		// 4. print the mouseColor variable to see what color the mouse is touching
+		// System.out.println(mouseColor);
+		// 5. make a variable to hold the background color.
+		int background = Color.BLACK.getRGB();
+		int mazeColor = maze.getRGB(mouseX, mouseY);
+		// 6. if the mouse falls off the path (if it is on the background)
+		if (mazeColor == background) {
+			scare();
+		}
+		// call the scare method
 
-		//5. make a variable to hold the background color. 
+		// 10. if the mouse is on the end color
+		if (mazeColor == -16646276) {
+			System.out.println("You win");
+		}
+		// pop up a message to tell them they won
 
-		//6. if the mouse falls off the path (if it is on the background)
-		
-				// call the scare method
-		
-		//10. if the mouse is on the end color
-				
-				// pop up a message to tell them they won
-		
 	}
 
 	private void scare() {
 		System.out.println("BOO!");
-		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
-		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
-		
-		//8. play the scary sound. Hint: type "sound" and then a period.		
-		
-		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
+		// 7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on
+		// freesound.org. Log in as leagueofamazing/code4life.
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("screech.wav"));
+		sound.play();
+		// 8. play the scary sound. Hint: type "sound" and then a period.
+
+		// 9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
 
 	}
 
@@ -73,11 +81,12 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 
 	public static void main(String[] args) throws Exception {
 		SwingUtilities.invokeLater(new ScaryMaze());
+
 	}
 
 	@Override
 	public void run() {
-		JFrame frame = new JFrame("June's Scary Maze");
+		JFrame frame = new JFrame("The Scary Maze");
 		frame.add(this);
 		setPreferredSize(new Dimension(frameWidth, frameHeight));
 		frame.pack();
@@ -92,9 +101,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {
+	}
 
 }
-
-
-
